d8ce255a755c95ef6bc4dee2323c26e0eafefed1
siph <dawkins.chris.dev@gmail.com>
2022-05-13 15:16:01 -0600

    bugfix: types

--- src/colors.rs
+++ src/colors.rs
@@ -1,7 +1,7 @@
-pub const BLACK: u32 = "#000000";
-pub const GREY: u32 = "#808080";
-pub const WHITE: u32 = "#ffffff";
-pub const PURPLE: u32 = "#a020f0";
-pub const BLUE: u32 = "#0000ff";
-pub const RED: u32 = "#ff0000";
+pub const BLACK: &str = "#000000";
+pub const GREY: &str = "#808080";
+pub const WHITE: &str = "#ffffff";
+pub const PURPLE: &str = "#a020f0";
+pub const BLUE: &str = "#0000ff";
+pub const RED: &str = "#ff0000";
 pub const GOKU: &str = "#f0833a";
